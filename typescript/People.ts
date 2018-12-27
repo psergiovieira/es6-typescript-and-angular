71cd648d99baa6c68b38769d7372509cc8fe649c
Paulo Sérgio Vieira <paulosv@cit.local>
2018-12-27 10:13:53 -0200

    Test with a module

---
+++ typescript/People.ts
@@ -0,0 +1,10 @@
+export class People{
+    private name: string;
+    public getName(): string{
+        return this.name;
+    } 
+
+    constructor(name:string){
+        this.name = name;
+    }
+}
--- typescript/car.ts
+++ typescript/car.ts
@@ -1,21 +1,26 @@
+import {  People } from "./People";
 class Car {    
     private model: string;
     private velocity: number = 0;
+    private owner: People;
 
     public speedUp(): void {
         this.velocity += 10;
     }
 
-    constructor(model:string){
+    constructor(model:string, owner:People){
         this.model = model;
+        this.owner = owner;
     }
 }
 
-let car = new Car("Ferrari");
+let owner = new People("Paulo");
+
+let car = new Car("Ferrari",owner);
 console.log(car);
 car.speedUp();
 car.speedUp();
 console.log(car);
 
-let carsList: Car[] = [new Car("Fusca"), new Car("Peugeot 207")];
+let carsList: Car[] = [new Car("Fusca",owner), new Car("Peugeot 207", owner)];
 console.log(carsList);
